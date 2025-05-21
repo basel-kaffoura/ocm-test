a4fc62697dc6bc3cd6817ae4e91323c205e729f7
basel-kaffoura <baselkaffoura@gmail.com>
2025-05-21 20:49:38 +0400

    Simple layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OCM Test</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
+    @vite(['resources/js/app.js'])
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('products.index') }}">Main Products</a>
+    </div>
+</nav>
+
+<div class="container mt-4">
+    @yield('content')
+</div>
+
+<footer class="bg-light py-4 mt-5">
+    <div class="container text-center">
+        <p class="mb-0">OCM Test, Copy @ {{ date('Y') }}</p>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,7 @@
-<h1>Products will be here</h1>
+@extends('layouts.app')
+
+@section('content')
+    <div id="app">
+        <h1>Products will be here</h1>
+    </div>
+@endsection
